1491f3f23bd216566f0cfbfca0c8d0bffb66463d
Saniha03 <138275063+Saniha03@users.noreply.github.com>
2025-08-18 01:47:49 +0600

    Update Routes.jsx

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes as RouterRoutes, Route } from "react-router-dom";
-import ScrollToTop from "components/ScrollToTop";
-import ErrorBoundary from "components/ErrorBoundary";
+import ScrollToTop from "./components/ScrollToTop";
+import ErrorBoundary from "./components/ErrorBoundary";
 import NotFound from "pages/NotFound";
 import LoginScreen from './pages/login-screen';
 import DailyTasks from './pages/daily-tasks';
