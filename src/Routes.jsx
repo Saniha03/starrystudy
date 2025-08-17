43965b659b567650cf76224b6fa342c895ebea68
Saniha03 <138275063+Saniha03@users.noreply.github.com>
2025-08-18 02:12:11 +0600

    Update Routes.jsx

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes as RouterRoutes, Route, Navigate } from "react-router-dom";
-import { useAuth } from "./context/AuthContext";
+import { useAuth } from "./contexts/AuthContext";
 
 import ScrollToTop from "./components/ScrollToTop";
 import ErrorBoundary from "./components/ErrorBoundary";
